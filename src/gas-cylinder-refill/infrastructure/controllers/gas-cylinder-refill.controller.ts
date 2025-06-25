7e6eddf21b861a4b6f8223801c0a8e77c427a974
Ronald Penaloza <ronald.dev86@gmail.com>
2025-06-24 23:12:37 -0400

    feature responseApi module gas-cylinder-refill

--- src/gas-cylinder-refill/infrastructure/controllers/gas-cylinder-refill.controller.ts
+++ src/gas-cylinder-refill/infrastructure/controllers/gas-cylinder-refill.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Put, Delete, Body, Param, Query } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Param, Query, HttpStatus, HttpException } from '@nestjs/common';
 import { CreateGasCylinderRefillUseCase } from '../../application/use-cases/create-gas-cylinder-refill.use-case';
 import { GetAllGasCylinderRefillsUseCase } from '../../application/use-cases/get-all-gas-cylinder-refills.use-case';
 import { GetGasCylinderRefillByIdUseCase } from '../../application/use-cases/get-gas-cylinder-refill-by-id.use-case';
@@ -7,9 +7,12 @@ import { UpdateGasCylinderRefillUseCase } from '../../application/use-cases/upda
 import { DeleteGasCylinderRefillUseCase } from '../../application/use-cases/delete-gas-cylinder-refill.use-case';
 import { CreateGasCylinderRefillDto } from '../../application/dtos/create-gas-cylinder-refill.dto';
 import { UpdateGasCylinderRefillDto } from '../../application/dtos/update-gas-cylinder-refill.dto';
+import { BaseController } from '../../../shared/infrastructure/controllers/base.controller';
+import { GasCylinderRefillNotFoundException } from '../../domain/exceptions/gas-cylinder-refill-not-found.exception';
+import { InvalidGasCylinderRefillDataException } from '../../domain/exceptions/invalid-gas-cylinder-refill-data.exception';
 
 @Controller('gas-cylinder-refills')
-export class GasCylinderRefillController {
+export class GasCylinderRefillController extends BaseController {
   constructor(
     private readonly createGasCylinderRefillUseCase: CreateGasCylinderRefillUseCase,
     private readonly getAllGasCylinderRefillsUseCase: GetAllGasCylinderRefillsUseCase,
@@ -17,40 +20,77 @@ export class GasCylinderRefillController {
     private readonly getGasCylinderRefillsByCylinderIdUseCase: GetGasCylinderRefillsByCylinderIdUseCase,
     private readonly updateGasCylinderRefillUseCase: UpdateGasCylinderRefillUseCase,
     private readonly deleteGasCylinderRefillUseCase: DeleteGasCylinderRefillUseCase,
-  ) {}
+  ) {
+    super();
+  }
 
   @Post()
   async create(@Body() createGasCylinderRefillDto: CreateGasCylinderRefillDto) {
-    const gasCylinderRefill = await this.createGasCylinderRefillUseCase.execute(createGasCylinderRefillDto);
-    return gasCylinderRefill.toJSON();
+    try {
+      const gasCylinderRefill = await this.createGasCylinderRefillUseCase.execute(createGasCylinderRefillDto);
+      return this.responseCreated(gasCylinderRefill, 'Recarga de cilindro creada correctamente');
+    } catch (error) {
+      if (error instanceof InvalidGasCylinderRefillDataException) {
+        throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+      }
+      throw new HttpException('Error al crear recarga de cilindro', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   @Get()
   async findAll(@Query('cylinderId') cylinderId?: string) {
-    if (cylinderId) {
-      const refills = await this.getGasCylinderRefillsByCylinderIdUseCase.execute(cylinderId);
-      return refills.map(refill => refill.toJSON());
-    } else {
-      const refills = await this.getAllGasCylinderRefillsUseCase.execute();
-      return refills.map(refill => refill.toJSON());
+    try {
+      if (cylinderId) {
+        const refills = await this.getGasCylinderRefillsByCylinderIdUseCase.execute(cylinderId);
+        return this.responseSuccess(refills, 'Recargas de cilindro recuperadas correctamente por ID de cilindro');
+      } else {
+        const refills = await this.getAllGasCylinderRefillsUseCase.execute();
+        return this.responseSuccess(refills, 'Recargas de cilindro recuperadas correctamente');
+      }
+    } catch (error) {
+      throw new HttpException('Error al obtener recargas de cilindro', HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 
   @Get(':id')
   async findOne(@Param('id') id: string) {
-    const gasCylinderRefill = await this.getGasCylinderRefillByIdUseCase.execute(id);
-    return gasCylinderRefill.toJSON();
+    try {
+      const gasCylinderRefill = await this.getGasCylinderRefillByIdUseCase.execute(id);
+      return this.responseSuccess(gasCylinderRefill, 'Recarga de cilindro recuperada correctamente');
+    } catch (error) {
+      if (error instanceof GasCylinderRefillNotFoundException) {
+        throw new HttpException(error.message, HttpStatus.NOT_FOUND);
+      }
+      throw new HttpException('Error al obtener recarga de cilindro', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   @Put(':id')
   async update(@Param('id') id: string, @Body() updateGasCylinderRefillDto: UpdateGasCylinderRefillDto) {
-    const gasCylinderRefill = await this.updateGasCylinderRefillUseCase.execute(id, updateGasCylinderRefillDto);
-    return gasCylinderRefill.toJSON();
+    try {
+      const gasCylinderRefill = await this.updateGasCylinderRefillUseCase.execute(id, updateGasCylinderRefillDto);
+      return this.responseSuccess(gasCylinderRefill, 'Recarga de cilindro actualizada correctamente');
+    } catch (error) {
+      if (error instanceof GasCylinderRefillNotFoundException) {
+        throw new HttpException(error.message, HttpStatus.NOT_FOUND);
+      }
+      if (error instanceof InvalidGasCylinderRefillDataException) {
+        throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+      }
+      throw new HttpException('Error al actualizar recarga de cilindro', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   @Delete(':id')
   async remove(@Param('id') id: string) {
-    await this.deleteGasCylinderRefillUseCase.execute(id);
-    return { message: 'Recarga eliminada exitosamente' };
+    try {
+      await this.deleteGasCylinderRefillUseCase.execute(id);
+      return this.responseSuccess(null, 'Recarga de cilindro eliminada correctamente');
+    } catch (error) {
+      if (error instanceof GasCylinderRefillNotFoundException) {
+        throw new HttpException(error.message, HttpStatus.NOT_FOUND);
+      }
+      throw new HttpException('Error al eliminar recarga de cilindro', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 } 
